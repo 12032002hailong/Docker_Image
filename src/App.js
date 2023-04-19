9a5c84b03fc762916c5827de5eb6cc06b3334d25
Hai Longe <hailongkc1203@example.com>
2023-04-19 21:09:44 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import './App.scss';
+import ManageUser from './Content/ManageUser';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+const App = () => {
+  return (
+    <>
+      <ManageUser />
+      <ToastContainer
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+      />
+      {/* Same as */}
+      <ToastContainer />
+    </>
+  );
+}
+
+export default App;
